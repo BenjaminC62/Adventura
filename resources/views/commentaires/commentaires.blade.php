468d0319e53ac5d258bca7b41f6da9a7861fbe27
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-12-18 12:29:47 +0100

    #12 - mise en place du formulaire pour les commentaires

---
+++ resources/views/commentaires/commentaires.blade.php
@@ -0,0 +1,12 @@
+<x-app>
+    <div> <!-- Pour flex s'il faut -->
+        <form>
+            <ul>
+                <li>
+                    <label for="contenu">Nom</label>
+                    <input type="text" id="contenu" name="contenu" required>
+                </li>
+            </ul>
+        </form>
+    </div>
+</x-app>
