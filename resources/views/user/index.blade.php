67b27d0a991bc91f8f5e5e3e18f8e0859ce43346
mathias_morel <mathias_morel@ens.univ-artois.fr>
2024-12-18 16:30:33 +0100

    #5 - Modif index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,6 +4,7 @@
 <ul>
     @foreach($users as $user)
         <li>
+            <img src="{{ $user->avatar }}" alt="Avatar de {{ $user->name }}" style="width: 50px; height: 50px; border-radius: 50%;">
             {{ $user->name }}
             <a href="{{ route('users.show', $user->id) }}" class="btn btn-info">Voir</a>
         </li>
