ad50a9b63a7c1b307596835c3691ea1df256b77d
matheo_dalmasso <matheo_dalmasso@ens.univ-artois.fr>
2024-12-18 20:15:44 +0100

    Modif du composant app

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,19 +1,21 @@
-<div>
-    <a href="{{route('accueil')}}">Accueil</a>
-    <a href="{{route('voyage.index')}}">Destination</a>
+<nav>
+    <div>
+        <a href="{{route('accueil')}}">Accueil</a>
+        <a href="{{route('voyage.index')}}">Destination</a>
 
-    <a href="#">Profil</a>
-    <a href="#">A propos</a>
-    <a href="#">Contact</a>
+        <a href="#">Profil</a>
+        <a href="#">A propos</a>
+        <a href="#">Contact</a>
 
-    @auth
-        {{Auth::user()->name}}
-        <a href="{{route("logout")}}"
-           onclick="document.getElementById('logout').submit(); return false;">Logout</a>
-        <form id="logout" action="{{route("logout")}}" method="post">
-            @csrf
-        </form>
-    @else
-        <a href="{{route("login")}}">Connexion</a>
-    @endauth
-</div>
+        @auth
+            {{Auth::user()->name}}
+            <a href="{{route("logout")}}"
+               onclick="document.getElementById('logout').submit(); return false;">Logout</a>
+            <form id="logout" action="{{route("logout")}}" method="post">
+                @csrf
+            </form>
+        @else
+            <a href="{{route("login")}}">Connexion</a>
+        @endauth
+    </div>
+</nav>
