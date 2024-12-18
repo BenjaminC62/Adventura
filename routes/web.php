f95c2e129d9d6852cfde18ab04e1f2c6d6e0d543
simon_caillieret <simon_caillieret@ens.univ-artois.fr>
2024-12-18 15:02:50 +0100

    creation du CRUD etapes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EtapeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,4 +22,4 @@ Route::get('/home', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name("dashboard") -> middleware('auth');
-
+Route::resource('etape', EtapeController::class);
