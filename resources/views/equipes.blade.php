9190071f6b1a49828f445c09fa3104a9d31a95fa
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-12-19 16:54:35 +0100

    mise en place du button de la vidéo

--- resources/views/equipes.blade.php
+++ resources/views/equipes.blade.php
@@ -1,4 +1,3 @@
-<!-- resources/views/equipes.blade.php -->
 <x-app>
     <div class="team-info">
         <h1>Notre Équipe</h1>
@@ -7,5 +6,7 @@
                 <h2>{{ $member }}</h2>
             </div>
         @endforeach
+        <a href="{{ route('video') }}" class="btn btn-primary" style="background-color: var(--brown); color: white; margin-top: 5px">Voir la vidéo</a>
     </div>
+
 </x-app>
