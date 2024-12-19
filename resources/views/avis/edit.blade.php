1cbe6b9a1ae9494028ba11347ca31b33a7b9377f
mathias_morel <mathias_morel@ens.univ-artois.fr>
2024-12-19 08:13:26 +0100

    AJout de css pour avisEdit

--- resources/views/avis/edit.blade.php
+++ resources/views/avis/edit.blade.php
@@ -1,13 +1,17 @@
-<div>
-    <form action="{{ route('avis.update', ['voyage' => $voyage, 'avis' => $avis]) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <ul>
-            <li>
-                <label for="contenu">Contenu</label>
-                <input type="text" id="contenu" name="contenu" value="{{ $avis->contenu }}" required>
-            </li>
-        </ul>
-        <button type="submit">Mettre à jour</button>
-    </form>
-</div>
+<!-- resources/views/avis/edit.blade.php -->
+<x-app>
+    <div class="form-container">
+        <p class="message">Modification d'un avis</p>
+        <form action="{{ route('avis.update', ['voyage' => $voyage, 'avis' => $avis]) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <ul>
+                <li>
+                    <label for="contenu">Contenu</label>
+                    <input type="text" id="contenu" name="contenu" value="{{ $avis->contenu }}" required>
+                </li>
+            </ul>
+            <button type="submit">Mettre à jour</button>
+        </form>
+    </div>
+</x-app>
