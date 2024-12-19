bd742e673760fae902884823315e2fb0571854be
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-12-19 14:15:58 +0100

    ajout de certains droits

--- resources/views/components/voyage-car-no-user.blade.php
+++ resources/views/components/voyage-car-no-user.blade.php
@@ -17,15 +17,22 @@
                                 <path d="M12 10c-1.104 0-2-.896-2-2s.896-2 2-2 2 .896 2 2-.896 2-2 2m0-5c-1.657 0-3 1.343-3 3s1.343 3 3 3 3-1.343 3-3-1.343-3-3-3m-7 2.602c0-3.517 3.271-6.602 7-6.602s7 3.085 7 6.602c0 3.455-2.563 7.543-7 14.527-4.489-7.073-7-11.072-7-14.527m7-7.602c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602"/>
                             </svg>
                             <span class="profil-titre-image">{{ $voyage->titre }}</span>
+                            @guest
+                            @else
                             <p>{{ $voyage->likes()->count() }} likes</p>
+                            @endguest
                         </div>
                         <div style="display: flex; margin-bottom: 20px">
+                            @can('update', $voyage)
                             <a href="{{ route('voyage.edit', $voyage->id) }}" style="width: 100%; height: 100%; object-fit: cover; color: white; padding: 10px 20px; background-color: var(--brown); border-radius: 5px; font-size: 1rem; border: none; cursor: pointer; transition: background-color 0.3s ease; margin-right: 10px;">Modifier</a>
+                            @endcan
+                            @can('destoy', $voyage)
                             <form action="{{ route('voyage.destroy', $voyage->id) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit">Supprimer</button>
                             </form>
+                            @endcan
                             <form action="{{ route('voyage.like', $voyage->id) }}" method="POST">
                                 @csrf
                                 <button type="submit">
